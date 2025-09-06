1e88a3067523df4c842309968bed171f0f2b5601
aftb777 <aftaabmulla228@gmail.com>
2025-09-06 11:43:58 +0530

    Created Swift Data Model

--- Income/Model/TransactionModel.swift
+++ Income/Model/TransactionModel.swift
@@ -6,6 +6,37 @@
 
 import Foundation
 import SwiftUI
+import SwiftData
+
+@Model class TransactionModel {
+    
+    var id = UUID()
+    var title: String
+    var type: TransactionType
+    var amount: Double
+    var date: Date
+    
+    init(id : UUID, title: String, type: TransactionType, amount: Double, date: Date){
+        self.id = id
+        self.title = title
+        self.type = type
+        self.amount = amount
+        self.date = date
+    }
+    
+    var displayDate: String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .short
+        return dateFormatter.string(from: date)
+    }
+    
+    func display(currency: Currency) -> String {
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .currency
+        numberFormatter.locale = currency.locale
+        return numberFormatter.string(from: amount as NSNumber) ?? ""
+    }
+}
 
 struct Transaction: Identifiable {
     
