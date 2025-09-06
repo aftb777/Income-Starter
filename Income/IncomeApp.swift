84614b6411cb3e463f94c3483a83913da5490484
aftb777 <aftaabmulla228@gmail.com>
2025-09-06 12:11:23 +0530

    Created Swift Data Container

--- Income/IncomeApp.swift
+++ Income/IncomeApp.swift
@@ -5,19 +5,29 @@
 //
 
 import SwiftUI
+import CoreData
+import SwiftData
 
 @main
 struct IncomeApp: App {
     
-    // create instance of DataManager
+    // create instance of DataManager (CoreData)
     let dataManager = DataManager()
     
     var body: some Scene {
         WindowGroup {
             HomeView()
+            // Container for core data
             // Pass Core Data context (viewContext) to SwiftUI environment
             // Now ANY child view can access Core Data using @Environment(\.managedObjectContext)
                 .environment(\.managedObjectContext, dataManager.container.viewContext)
+            
+            
+            // MARK : Container for SwiftData
+            // pass @Swift Model name.self here
+                .modelContainer(for : [
+                    TransactionModel.self
+                ])
         }
     }
 }
